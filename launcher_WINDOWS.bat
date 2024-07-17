f0d7718c972a71c32b75f7cdde2e11ed9b30fe24
Fellshadow <106463662+Fellshadow@users.noreply.github.com>
2024-07-17 01:30:47 +0100

    update launcher error check
    Win11 has dummy python exes that make us sadge

--- launcher_WINDOWS.bat
+++ launcher_WINDOWS.bat
@@ -2,14 +2,21 @@
 @REM Windows launcher for the citra tracker
 
 @REM Run the version check script to test if 3.12 is installed
-call python util\python-version-check.py 2> nul
+call python util\python-version-check.py
 
-@REM ERROR LEVELS: 0 - Success, 1 - Fail (python not found), 2 - Fail (incorrect python version)
-if %ERRORLEVEL%==1 (
+@REM ERROR LEVELS: 0 - Success, 1 - Fail (python not found), 2 - Fail (incorrect python version), 9009 - Windows 11 being dumb probably
+if %ERRORLEVEL%==9009 (
+   @REM Windows 11 throws a 9009 instead of 1 if the dummy python exe is ran instead (thanks Microsoft)
+   echo Python is not installed.
+   echo Python 3.12 is required to run the tracker. Install from https://www.python.org/
+   echo Make sure to check the "Add python.exe to PATH" box in the installer.
+   echo You may also need to turn off python under App execution aliases in your Windows settings, these are dummy Python files Windows comes with and can block the installed version from python.org
+) else if %ERRORLEVEL%==1 (
    @REM Python not found on PATH
    echo Python is not installed or the PATH was not set during installation.
    echo Python 3.12 is required to run the tracker. Install from https://www.python.org/
    echo Make sure to check the "Add python.exe to PATH" box in the installer.
+   echo If you are on Windows 11, you may also need to turn off python under App execution aliases in your Windows settings.
 ) else if %ERRORLEVEL%==0 (
    @REM Run the tracker
    echo Launching the Citra Tracker...
